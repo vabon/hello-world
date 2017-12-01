f58c1dc236b199dfa12fd61cf9d6d2507d734149
vabon <bucks7abu@gmail.com>
2017-12-01 13:02:01 +0300

    Add a question to main.cpp
    Add a question about user's name

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, const char * argv[])
 {
+    string username;
+    
     cout << "Hello world!" << endl;
+    cout << "What's your name?" << endl;
+    cin >> username;
+    cout << "Hi, " << username << "!" << endl;
     
     return 0;
 }
