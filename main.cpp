457f9fbf7e807b272ed0329c6ede4c999089eef0
vabon <bucks7abu@gmail.com>
2017-12-01 13:30:58 +0300

    Update main.cpp
    Insert a question about user's dog

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, const char * argv[])
 {
+    string dogname;
+    
     cout << "Hello world!" << endl;
+    cout << "What\'s your dog\'s name?" << endl;
+    cin >> dogname;
+    cout << "Oh, " << dogname << " is a good name for dog!" << endl;
     
     return 0;
 }
