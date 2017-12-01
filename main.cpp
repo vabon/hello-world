bdec51b32c2f5e8a575ad9950cb1c4217f7f6ce4
vabon <bucks7abu@gmail.com>
2017-12-01 13:47:44 +0300

    Update main.cpp
    Question about age

--- main.cpp
+++ main.cpp
@@ -6,11 +6,15 @@ using namespace std;
 int main(int argc, const char * argv[])
 {
     string username;
+    int age;
     
     cout << "Hello world!" << endl;
     cout << "What's your name?" << endl;
     cin >> username;
     cout << "Hi, " << username << "!" << endl;
+    cout << "How old are you?" << endl;
+    cin >> age;
+    cout << "You're " << age << " years old, nice." << endl;
     
     return 0;
 }
